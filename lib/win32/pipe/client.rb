629a723b494f1c3b13f507c98aae331ee6aef2d7
Daniel Berger <djberg96@gmail.com>
2010-07-23 17:42:12 -0700

    Formatting and comments.

--- lib/win32/pipe/client.rb
+++ lib/win32/pipe/client.rb
@@ -1,49 +1,65 @@
 # The Win32 module serves as a namespace only
 module Win32
-   # The Pipe::Client class encapsulates the client side of a named pipe
-   # connection.
-   #
-   class Pipe::Client < Pipe
-      # Create and return a new Pipe::Client instance.
-      #
-      # The default pipe mode is PIPE_WAIT.
-      #
-      # The default open mode is FILE_ATTRIBUTE_NORMAL | FILE_FLAG_WRITE_THROUGH.
-      #--
-      # 2147483776 is FILE_ATTRIBUTE_NORMAL | FILE_FLAG_WRITE_THROUGH
-      def initialize(name, pipe_mode = DEFAULT_PIPE_MODE, open_mode = DEFAULT_OPEN_MODE)
-         super(name, pipe_mode, open_mode)
-         
-         @pipe = CreateFile(
-            @name,
-            GENERIC_READ | GENERIC_WRITE,
-            FILE_SHARE_READ | FILE_SHARE_WRITE,
-            nil,
-            OPEN_EXISTING,
-            @open_mode,
-            nil 
-         )
 
-         error = GetLastError()
+  # The Pipe::Client class encapsulates the client side of a named pipe
+  # connection.
+  #
+  class Pipe::Client < Pipe
+
+    # Create and return a new Pipe::Client instance. The +name+, +pipe_mode+,
+    # and +open_mode+ are passed to the Win32::Pipe superclass.
+    #
+    # The default +pipe_mode+ is NOWAIT.
+    #
+    # The default +open_mode+ is FILE_ATTRIBUTE_NORMAL | FILE_FLAG_WRITE_THROUGH.
+    #
+    # In block form the client object is yield, and is automatically
+    # disconnected and closed at the end of the block.
+    #
+    # Example:
+    #
+    #   require 'win32/pipe'
+    #
+    #   Pipe::Client.new('foo') do |pipe|
+    #     puts "Connected..."
+    #     pipe.write("Ruby rocks!")
+    #     data = pipe.read
+    #     puts "Got [#{data}] back from pipe server"
+    #  end
+    #
+    def initialize(name, pipe_mode = DEFAULT_PIPE_MODE, open_mode = DEFAULT_OPEN_MODE)
+      super(name, pipe_mode, open_mode)
          
-         if error == ERROR_PIPE_BUSY
-            unless WaitNamedPipe(@name, NMPWAIT_WAIT_FOREVER)
-               raise Error, get_last_error
-            end
-         end
+      @pipe = CreateFile(
+        @name,
+        GENERIC_READ | GENERIC_WRITE,
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        nil,
+        OPEN_EXISTING,
+        @open_mode,
+        nil 
+      )
+
+      error = GetLastError()
          
-         if @pipe == INVALID_HANDLE_VALUE
-            raise Error, get_last_error
-         end
-         
-         if block_given?
-            begin
-               yield self
-            ensure
-               disconnect
-               close
-            end
-         end
+      if error == ERROR_PIPE_BUSY
+        unless WaitNamedPipe(@name, NMPWAIT_WAIT_FOREVER)
+          raise Error, get_last_error
+        end
       end
-   end
+         
+      if @pipe == INVALID_HANDLE_VALUE
+        raise Error, get_last_error
+      end
+         
+      if block_given?
+        begin
+          yield self
+        ensure
+          disconnect
+          close
+        end
+      end
+    end
+  end
 end
